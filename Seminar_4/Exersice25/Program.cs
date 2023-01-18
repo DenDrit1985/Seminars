a6f1570aac8bee71b15f348f322192d394b0b044
NedoshivinDV <9270534814@rambler.ru>
2023-01-19 00:02:06 +0300

    домашнее задание 4 сделано

---
+++ Seminar_4/Exersice25/Program.cs
@@ -0,0 +1,25 @@
+﻿/*
+Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.
+*/
+int ToDegree(int numberA, int numberB)
+// Функция возведения в степень
+{
+    int result = numberA;
+    for (int idx = 1; idx < numberB; idx++)
+    {
+        result *= numberA;
+    }
+    return result;
+}
+// Функция ввода
+int ReadInt()
+{
+    Console.WriteLine("Введите число A:");
+    int numberA = Convert.ToInt32(Console.ReadLine());
+    Console.WriteLine("Введите число B:");
+    int numberB = Convert.ToInt32(Console.ReadLine());
+    return ToDegree(numberA, numberB);
+}
+
+int res = ReadInt();
+Console.Write(res);
---
+++ Seminar_4/Exersice29/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+Задача 29:
+Напишите программу, которая задаёт массив из 8 элементов и выводит их на экран. Данные вводятся с консоли пользователем
+*/
+int[] randArr(int[] smArry)
+{
+    for(int idx = 0; idx < smArry.Length; idx++)
+    {
+        Console.WriteLine($"Введите {idx + 1} значение: ");
+        int nextNum = Convert.ToInt32(Console.ReadLine());
+        smArry[idx] = nextNum;
+    }
+    return smArry;
+}
+
+void prntArry(int[] array)
+{
+    for(int idx = 0; idx < array.Length; idx++)
+    {
+        Console.Write($"{array[idx]}, ");
+    }
+}
+
+int[] array_1 = new int[8];
+
+randArr(array_1);
+prntArry(array_1);
